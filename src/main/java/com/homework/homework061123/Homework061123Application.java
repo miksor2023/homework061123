c2305f299ee8acb56b5bc72c82dadfcb3845c6e2
miksor2023 <miksor@yandex.ru>
2023-11-09 15:09:47 +0300

    Initial commit

---
+++ src/main/java/com/homework/homework061123/Homework061123Application.java
@@ -0,0 +1,13 @@
+package com.homework.homework061123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework061123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework061123Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/homework/homework061123/Homework061123ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework.homework061123;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework061123ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
